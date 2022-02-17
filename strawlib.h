a0913919a83734cf2473d67021ca32f588cc6a8a
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-02-17 17:58:11 +0100

    Add files via upload

---
+++ strawlib.h
@@ -0,0 +1,9 @@
+#ifndef STRAWLIB_H
+#define STRAWLIB_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#endif /* STRAWLIB_H */
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <strawlib/strawlib.h>
+
+
+int main(void) {
+    return 0;
+}
